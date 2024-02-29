323fc598d13848785037b0875cf8079ac8d9b213
SweetJellyQwQ <577430840@qq.com>
2024-02-29 14:37:13 +0800

    use GM_addElement

--- youdao-web-plugin.js
+++ youdao-web-plugin.js
@@ -5,15 +5,17 @@
 // @description  Easy to use
 // @author       jellyqwq
 // @match        https://*.youdao.com/**/**
-// @icon         none
-// @grant        none
-// @require      
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youdao.com
+// @grant        GM_addElement
+// @require      https://raw.githubusercontent.com/jellyqwq/youdao-web-plugin/main/youdao-web-plugin.src.js#sha256=35303331323130333544373636423435454244373530413236353941354646314530343046453241384530444634383844323533423943463545314135323446
+
+
 // ==/UserScript==
 
 (function() {
     'use strict';
     GM_addElement('script', {
-        src: "",
+        src: "https://raw.githubusercontent.com/jellyqwq/youdao-web-plugin/main/youdao-web-plugin.src.js",
         type: 'text/javascript'
     })
 })
