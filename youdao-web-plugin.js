2c68929378273ba17f1e2da76097e0bc354ed608
SweetJellyQwQ <577430840@qq.com>
2024-02-29 14:26:48 +0800

    add src file

--- youdao-web-plugin.js
+++ youdao-web-plugin.js
@@ -7,37 +7,14 @@
 // @match        https://*.youdao.com/**/**
 // @icon         none
 // @grant        none
+// @require      
 // ==/UserScript==
 
 (function() {
     'use strict';
-    function clickAudio (latestWord) {
-        const timerId = setInterval(function(){
-            if (latestWord != document.getElementsByClassName("title")[0].childNodes[0].textContent) {
-                let audioElement = document.getElementsByClassName("title")[0].getElementsByClassName("pronounce")
-                if (audioElement.length == 1) {
-                    audioElement[0].click()
-                } else {
-                    document.getElementsByClassName("per-phone")[1].getElementsByClassName("pronounce")[0].click()
-                }
-                clearInterval(timerId);
-            }
-        }, 500)
-    }
-    const wait = (milliseconds) => new Promise(resolve => setTimeout(resolve, milliseconds));
-    document.addEventListener('keydown', function(event) {
-        // 检查按下的键是否是特定的快捷键
-        if (event.key === 'Enter') {
-            let latestWord = document.getElementsByClassName("title")[0].childNodes[0].textContent
-            if (latestWord == document.getElementById("search_input").value) {
-                document.getElementById("search_input").focus()
-                return
-            }
-            async function doSomethingAfterDelay() {
-                await wait(500);
-                clickAudio(latestWord)
-            }doSomethingAfterDelay();
-        }
-    });
+    GM_addElement('script', {
+        src: "",
+        type: 'text/javascript'
+    })
 })
     
