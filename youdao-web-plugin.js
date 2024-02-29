e93a8733ad2ae3520ce2392168d9727138744a2c
SweetJellyQwQ <577430840@qq.com>
2024-02-29 14:48:04 +0800

    fixed match bug

--- youdao-web-plugin.js
+++ youdao-web-plugin.js
@@ -3,10 +3,10 @@
 // @namespace    https://github.com/jellyqwq
 // @downloadURL  https://raw.githubusercontent.com/jellyqwq/youdao-web-plugin/main/youdao-web-plugin.js
 // @updateURL    https://raw.githubusercontent.com/jellyqwq/youdao-web-plugin/main/youdao-web-plugin.js
-// @version      0.2.1
+// @version      0.2.2
 // @description  Easy to use
 // @author       jellyqwq
-// @match        https://*.youdao.com/**/**
+// @match        https://www.youdao.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youdao.com
 // @grant        GM_addElement
 // @require      https://raw.githubusercontent.com/jellyqwq/youdao-web-plugin/main/youdao-web-plugin.src.js#sha256=35303331323130333544373636423435454244373530413236353941354646314530343046453241384530444634383844323533423943463545314135323446
